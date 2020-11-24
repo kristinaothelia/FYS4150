ef343c0d3fbbe6d9afa69e43317d5ca36c57527a
kristinaothelia <>
2020-11-24 14:57:45 +0100

    New d) figures

--- Project4/new_main.py
+++ Project4/new_main.py
@@ -1,8 +1,9 @@
 import sys, os, time, argparse
 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy             as np
 import pandas            as pd
+import plots             as P
 
 from   numba import jit, njit, prange, set_num_threads
 
@@ -231,173 +232,24 @@ def two_temps(L, n_cycles, temp):
 
     return E, Mag, MagAbs, SH, Suscept, Naccept
 
-def plot_expected_net_mag(L, temp, runs):
-    """
-    plotting expected net mag
-
-    the plots show that value goes to zero (expected value)
-    for large (1e7) number of mc-cycles.
-
-    inspo from rapport (u know who)...
-
-    should probably increase N...?
-    not sure what it 'should be' or why...
-    """
-
-    colors      = ['rosybrown','lightcoral','indianred','firebrick','darkred','red']
-    spin_matrix = np.ones((L, L), np.int8)
-
-    plt.figure(figsize=(10, 6))
-
-    N     = 30  # number of times to run n_cycles
-    count = 0
-
-    for n_cycles in runs:
-
-        c     = colors[count]
-        count += 1
-        for i in range(N):
-
-            E, Mag, MagAbs, SH, Suscept, Naccept = numerical_solution(spin_matrix, int(n_cycles), temp, L)
-            plt.semilogx(int(n_cycles), Mag, 'o', color=c)
-
-    plt.title('Spread of Expected Magnetic Field of Matrix', fontsize=15)
-    plt.xlabel('Number of Monte-Carlo Cycles', fontsize=15)
-    plt.ylabel(r'\langle M \rangle', fontsize=15)
-    plt.xticks(fontsize=12);plt.yticks(fontsize=12)
-    plt.savefig(f'results/plots/4c/SpreadOfExpectedMagneticField')
-    plt.show()
-
-def plot_MCcycles_vs_err(mc_cycles, error):
-    """Plotting error vs. number of MC cycles.
-
-    loglog or semilog?
-    Need better adjustment of plot.
-    New title, xlabel, ylabel etc.
-    """
-    plt.figure(figsize=(15, 10))
-
-    #plt.loglog(mc_cycles, error, 'bo-')
-    plt.semilogx(mc_cycles, error, 'bo-') # or loglog?
-
-    # zip joins x and y coordinates in pairs
-    for x,y in zip(mc_cycles,error):
-
-        label = f'{y:10.2e}'
-
-        plt.annotate(label, # this is the text
-                     (x,y), # this is the point to label
-                     textcoords="offset points", # how to position the text
-                     xytext=(0,-10), # distance from text to points (x,y)
-                     color='black',
-                     weight='bold',
-                     size='smaller',
-                     rotation='0',   # plot seems weird w/angle other than 0 or 360..?
-                     va='top',       #  [ 'center' | 'top' | 'bottom' | 'baseline' ]
-                     ha='right')     #  [ 'left' | 'right' | 'center']
-
-    xmin = 0.50e2 #f'{np.min(mc_cycles):10.2e}'  #0.5e2
-    xmax = 1.5e7  #f'{np.max(mc_cycles):10.2e}'  #1.1e7
-    #plt.ylim(error.min(), error.max()); #plt.ylim(1e-6, 1e-3)
-    #plt.xlim(xmin, xmax)
-
-    plt.title('Error of the Mean Abs. Magnetization',fontsize=15)
-    plt.xlabel('Number of Monte-Carlo Cycles',fontsize=15)
-    plt.ylabel('error',fontsize=15)
-    plt.xticks(fontsize=12);plt.yticks(fontsize=12)
-    plt.tight_layout()
-    plt.savefig(f'results/plots/4c/ErrorMeanMagnetizationAbs')
-    plt.show()
 
 
-def expected_vals_two_temp(MCcycles, T1, T2, expected):
-    """
-    Function for plotting expectation values vs. MC cycles,
-    - two temperatures in two different initial states.
-
-    expected has structure [temp, physical quantity, ...]
-    """
-
-    names     = ['Energy','Magnetization','Abs. Magnetization',\
-                 'Specific Heat','Susceptibility']
-
-    ylabels   = [r'$\langle E\rangle$', r'$\langle M \rangle$',\
-                 r'$\langle|M|\rangle$', r'$C_P$', r'$\chi$']
-
-    save_as   = ['energy','mag','Mabs','CP','CHI']
-
-    x = np.linspace(1,MCcycles,MCcycles, endpoint=True).astype(np.float_)
-
-    for i, val in enumerate(expected):
-        print(names[i])
-        plt.figure(figsize=(7,5))
-        plt.semilogx(x, val[0,0,:], linewidth=1.0,\
-                                    label=f'T={T1} (order)',\
-                                    color='tab:blue')
-        plt.semilogx(x, val[0,1,:], linewidth=1.0,\
-                                    label=f'T={T2} (order)',\
-                                    color='tab:red')
-
-        plt.semilogx(x, val[1,0,:], '--', linewidth=1.0,\
-                                          label=f'T={T1} (disorder)',\
-                                          color='tab:blue')
-        plt.semilogx(x, val[1,1,:], '--', linewidth=1.0,\
-                                          label=f'T={T2} (disorder)',\
-                                          color='tab:red')
-
-        plt.title(f'Expectation Values of {names[i]}', fontsize=15)
-        plt.xlabel('Number of Monte-Carlo Cycles', fontsize=15)
-        plt.ylabel(f'{ylabels[i]}', fontsize=15)
-        plt.xticks(fontsize=12);plt.yticks(fontsize=12)
-        plt.legend(fontsize=12)
-        plt.tight_layout()
-        plt.savefig(f'results/plots/4d/expected_{save_as[i]}_{MCcycles}')
-        #plt.show()
-
-def plot_n_accepted(MCcycles, Naccs):
-
-    print(Naccs)
-    print(Naccs.shape)
-
-    order_T1 = Naccs[0,0,:]
-    print(np.sum(order_T1))
-
-    x  = np.linspace(1,MCcycles,MCcycles, endpoint=True).astype(np.float_)
-    na = np.linspace(1,MCcycles,MCcycles, endpoint=True)
-
-    plt.figure(figsize=(7,5))
-
-    plt.plot(x, Naccs[0,0,:],label=f'T={T1} (order)',color='tab:blue')
-    plt.plot(x, Naccs[0,1,:],label=f'T={T2} (order)',color='tab:red')
-    plt.plot(x, Naccs[1,0,:], '--',label=f'T={T1} (disorder)',color='tab:blue')
-    plt.plot(x, Naccs[1,1,:], '--',label=f'T={T2} (disorder)',color='tab:red')
-
-    plt.title('Total Number of Accepted Configurations', fontsize=15)
-    plt.xlabel('Number of Monte-Carlo Cycles', fontsize=15)
-    plt.ylabel('Number of Accepted Configurations', fontsize=15)
-    plt.xticks(fontsize=12);plt.yticks(fontsize=12)
-    plt.legend(fontsize=12)
-    plt.tight_layout()
-    plt.savefig(f'results/plots/4d/AcceptedConfigs')
-    plt.show()
-
-    #sys.exit()
 
 
 ex_c = False
-ex_d = False
-ex_e = False
-
-#ex_c = True
 ex_d = True
+ex_e = False
+ex_f = False
+ex_g = False
 
-# Initial conditions
-max_cycles = 1e7          # Max. MC cycles
-max_cycles = 10000000
 
 #set_num_threads(3)
 
 if ex_c:
+
+    # Initial conditions
+    max_cycles = 1e7          # Max. MC cycles
+    max_cycles = 10000000
     L          = 2            # Number of spins
     temp       = 1            # [kT/J] Temperature
     J          = 1
@@ -436,7 +288,7 @@ if ex_c:
     if expected_net_magnetism:
 
         # Plotting expected mean magnetism
-        plot_expected_net_mag(L, temp, runs=log_scale)
+        P.plot_expected_net_mag(L, temp, runs=log_scale)
 
 
 if ex_d:
@@ -450,22 +302,37 @@ if ex_d:
 
     E, Mag, MagAbs, SH, Suscept, n_acc = two_temps(L, MC_runs, temp_arr)
 
-    plot_n_accepted(MC_runs, n_acc)
+    P.plot_n_accepted(MC_runs, n_acc, T1, T2)
 
     expecteds = [E, Mag, MagAbs, SH, Suscept]
-    expected_vals_two_temp(MC_runs, T1, T2, expecteds)
+    P.expected_vals_two_temp(MC_runs, T1, T2, expecteds)
 
 if ex_e:
-    #Partition function:
-    #It is a sum over the two possible spin values for each spon, either up +1 or down −1.
-
+    """
+    Partition function:
+    It is a sum over the two possible spin values for each spon, either up +1 or down −1.
+    """
     L  = 20    # Number of spins
     T1 = 1.0   # [kT/J] Temperature
     T2 = 2.4   # [kT/J] Temperature
 
     temp_arr = np.array([T1, T2])
-    MC_runs  = 10000
+    MC_runs  = int(1e6)
 
 
     #Tenkte å prøve og skrive om dette eller noe kanskje..
     #https://github.com/siljeci/FYS4150/blob/master/Project4/CODES/prob.py
+
+if ex_f:
+    """
+    skriv noe
+    """
+    L  = [40, 60, 80, 100]    # Number of spins
+    T1 = 2.0   # [kT/J] Temperature
+    T2 = 2.3   # [kT/J] Temperature
+    dT = 0.05
+
+if ex_g:
+    """
+    Find T_C
+    """
---
+++ Project4/plots.py
@@ -0,0 +1,157 @@
+"""
+Various plotting functions used in main.py
+"""
+
+import matplotlib.pyplot as plt
+import numpy             as np
+
+def plot_expected_net_mag(L, temp, runs):
+    """
+    plotting expected net mag
+
+    the plots show that value goes to zero (expected value)
+    for large (1e7) number of mc-cycles.
+
+    inspo from rapport (u know who)...
+
+    should probably increase N...?
+    not sure what it 'should be' or why...
+    """
+
+    colors      = ['rosybrown','lightcoral','indianred','firebrick','darkred','red']
+    spin_matrix = np.ones((L, L), np.int8)
+
+    plt.figure(figsize=(10, 6))
+
+    N     = 30  # number of times to run n_cycles
+    count = 0
+
+    for n_cycles in runs:
+
+        c     = colors[count]
+        count += 1
+        for i in range(N):
+
+            E, Mag, MagAbs, SH, Suscept, Naccept = numerical_solution(spin_matrix, int(n_cycles), temp, L)
+            plt.semilogx(int(n_cycles), Mag, 'o', color=c)
+
+    plt.title('Spread of Expected Magnetic Field of Matrix', fontsize=15)
+    plt.xlabel('Number of Monte-Carlo Cycles', fontsize=15)
+    plt.ylabel(r'\langle M \rangle', fontsize=15)
+    plt.xticks(fontsize=13);plt.yticks(fontsize=13)
+    plt.savefig(f'results/plots/4c/SpreadOfExpectedMagneticField')
+    plt.show()
+
+def plot_MCcycles_vs_err(mc_cycles, error):
+    """Plotting error vs. number of MC cycles.
+
+    loglog or semilog?
+    Need better adjustment of plot.
+    New title, xlabel, ylabel etc.
+    """
+    plt.figure(figsize=(15, 10))
+
+    #plt.loglog(mc_cycles, error, 'bo-')
+    plt.semilogx(mc_cycles, error, 'bo-') # or loglog?
+
+    # zip joins x and y coordinates in pairs
+    for x,y in zip(mc_cycles,error):
+
+        label = f'{y:10.2e}'
+
+        plt.annotate(label, # this is the text
+                     (x,y), # this is the point to label
+                     textcoords="offset points", # how to position the text
+                     xytext=(0,-10), # distance from text to points (x,y)
+                     color='black',
+                     weight='bold',
+                     size='smaller',
+                     rotation='0',   # plot seems weird w/angle other than 0 or 360..?
+                     va='top',       #  [ 'center' | 'top' | 'bottom' | 'baseline' ]
+                     ha='right')     #  [ 'left' | 'right' | 'center']
+
+    xmin = 0.50e2 #f'{np.min(mc_cycles):10.2e}'  #0.5e2
+    xmax = 1.5e7  #f'{np.max(mc_cycles):10.2e}'  #1.1e7
+    #plt.ylim(error.min(), error.max()); #plt.ylim(1e-6, 1e-3)
+    #plt.xlim(xmin, xmax)
+
+    plt.title('Error of the Mean Abs. Magnetization',fontsize=15)
+    plt.xlabel('Number of Monte-Carlo Cycles',fontsize=15)
+    plt.ylabel('error',fontsize=15)
+    plt.xticks(fontsize=13);plt.yticks(fontsize=13)
+    plt.tight_layout()
+    plt.savefig(f'results/plots/4c/ErrorMeanMagnetizationAbs')
+    plt.show()
+
+
+def expected_vals_two_temp(MCcycles, T1, T2, expected):
+    """
+    Function for plotting expectation values vs. MC cycles,
+    - two temperatures in two different initial states.
+
+    expected has structure [temp, physical quantity, ...]
+    """
+
+    names     = ['Energy','Magnetization','Abs. Magnetization',\
+                 'Specific Heat','Susceptibility']
+
+    ylabels   = [r'$\langle E\rangle$', r'$\langle M \rangle$',\
+                 r'$\langle|M|\rangle$', r'$C_V$', r'$\chi$']
+
+    save_as   = ['energy','mag','Mabs','CV','CHI']
+
+    x = np.linspace(1,MCcycles,MCcycles, endpoint=True).astype(np.float_)
+
+    for i, val in enumerate(expected):
+
+        print("Saving plot for: ", names[i])
+
+        plt.figure(figsize=(7,5))
+        plt.semilogx(x, val[0,0,:], linewidth=1.0,\
+                                    label=f'T={T1} (order)',\
+                                    color='tab:blue')
+        plt.semilogx(x, val[0,1,:], linewidth=1.0,\
+                                    label=f'T={T2} (order)',\
+                                    color='tab:red')
+
+        plt.semilogx(x, val[1,0,:], '--', linewidth=1.0,\
+                                          label=f'T={T1} (disorder)',\
+                                          color='tab:blue')
+        plt.semilogx(x, val[1,1,:], '--', linewidth=1.0,\
+                                          label=f'T={T2} (disorder)',\
+                                          color='tab:red')
+
+        plt.title(f'Expectation Values of {names[i]}', fontsize=15)
+        plt.xlabel('Number of Monte-Carlo Cycles', fontsize=15)
+        plt.ylabel(f'{ylabels[i]}', fontsize=15)
+        plt.xticks(fontsize=13);plt.yticks(fontsize=13)
+        plt.legend(fontsize=13)
+        plt.tight_layout()
+        plt.savefig(f'results/plots/4d/expected_{save_as[i]}_{MCcycles}')
+        #plt.show()
+
+def plot_n_accepted(MCcycles, Naccs, T1, T2):
+
+    #print(Naccs)
+    #print(Naccs.shape)
+
+    order_T1 = Naccs[0,0,:]
+
+    x  = np.linspace(1,MCcycles,MCcycles, endpoint=True).astype(np.float_)
+    na = np.linspace(1,MCcycles,MCcycles, endpoint=True)
+
+    plt.figure(figsize=(7,5))
+
+    plt.plot(x, Naccs[0,0,:],label=f'T={T1} (order)',color='tab:blue')
+    plt.plot(x, Naccs[0,1,:],label=f'T={T2} (order)',color='tab:red')
+    plt.plot(x, Naccs[1,0,:], '--',label=f'T={T1} (disorder)',color='tab:blue')
+    plt.plot(x, Naccs[1,1,:], '--',label=f'T={T2} (disorder)',color='tab:red')
+
+    plt.title('Total Number of Accepted Configurations', fontsize=15)
+    plt.xlabel('Number of Monte-Carlo Cycles', fontsize=15)
+    plt.ylabel('Number of Accepted Configurations', fontsize=15)
+    plt.xticks(fontsize=13);plt.yticks(fontsize=13)
+    plt.legend(fontsize=13)
+    plt.tight_layout()
+    plt.savefig(f'results/plots/4d/AcceptedConfigs')
+    plt.show()
