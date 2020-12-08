5fd73445865eda3a157982c39a33d6edd9eb7804
kristinaothelia <>
2020-12-09 00:11:54 +0100

    Merge and plots

--- Project5/RK4_disease.py
+++ Project5/RK4_disease.py
@@ -2,7 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
-def RK4(b, x0, y0, fx, fy, n=None, dt=None, T=None):
+import plots as P
+
+def RK4(a, b, c, x0, y0, fx, fy, N, T, n):
     """
     fx = fS
     fy = fI
@@ -21,10 +23,17 @@ def RK4(b, x0, y0, fx, fy, n=None, dt=None, T=None):
 
     #loop for Runge-Kutta 4th Order
     for i in range(n-1):
-        kx1 = dt*fx(t[i], x[i], y[i]); ky1 = dt*fy(t[i], x[i], y[i], b)
-        kx2 = dt*fx(t[i] + dt/2, x[i] + kx1/2, y[i] + ky1/2); ky2 = dt*fy(t[i] + dt/2, x[i] + kx1/2, y[i] + ky1/2, b)
-        kx3 = dt*fx(t[i] + dt/2, x[i] + kx2/2, y[i] + ky2/2); ky3 = dt*fy(t[i] + dt/2, x[i] + kx2/2, y[i] + ky2/2, b)
-        kx4 = dt*fx(t[i] + dt, x[i] + kx3, y[i] + ky3); ky4 = dt*fy(t[i] + dt, x[i] + kx3, y[i] + ky3, b)
+        kx1 = dt*fx(a, c, t[i], x[i], y[i], N)
+        ky1 = dt*fy(a, b, t[i], x[i], y[i], N)
+
+        kx2 = dt*fx(a, c, t[i] + dt/2, x[i] + kx1/2, y[i] + ky1/2, N)
+        ky2 = dt*fy(a, b, t[i] + dt/2, x[i] + kx1/2, y[i] + ky1/2, N)
+
+        kx3 = dt*fx(a, c, t[i] + dt/2, x[i] + kx2/2, y[i] + ky2/2, N)
+        ky3 = dt*fy(a, b, t[i] + dt/2, x[i] + kx2/2, y[i] + ky2/2, N)
+
+        kx4 = dt*fx(a, c, t[i] + dt, x[i] + kx3, y[i] + ky3, N)
+        ky4 = dt*fy(a, b, t[i] + dt, x[i] + kx3, y[i] + ky3, N)
 
         x[i+1] = x[i] + (kx1 + 2*(kx2 + kx3) + kx4)/6
         y[i+1] = y[i] + (ky1 + 2*(ky2 + ky3) + ky4)/6
@@ -32,28 +41,23 @@ def RK4(b, x0, y0, fx, fy, n=None, dt=None, T=None):
 
     return x,y,t
 
-def fS(t, S, I):
+def fS(a, c, t, S, I, N):
     """
     Right hand side of S' = dS/dt
     """
     return c*(N - S - I) - a*S*I/N
 
-def fI(t, S, I, b):
+def fI(a, b, t, S, I, N):
     """
     Right hand side of I' = dI/dt
     """
     return a*S*I/N - b*I
 
+'''
 ###data
 a = 4               # Rate of transmission
 c = 0.5             # Rate of immunity loss
 b = 3
-
-bA      = 1         # Rate of recovery for population A
-bB      = 2         # Rate of recovery for population B
-bC      = 3         # Rate of recovery for population C
-bD      = 4         # Rate of recovery for population D
-b_list  = [bA, bB, bC, bD]
 
 T = 30  #days
 n = 1000
@@ -67,27 +71,5 @@ S, I, time = RK4(b, S_0, I_0, fS, fI, n, T=T)
 
 R = N - S - I
 
-def plot_SIR(time, b, S, I, R, T, method, save_plot=False):
-
-    plt.figure()
-    plt.plot(time, S, label="Susceptible")
-    plt.plot(time, I, label="Infected")
-    plt.plot(time, R, label="Recovered")
-    plt.legend(fontsize=15)
-    plt.title('??? b=%g' %b)
-    plt.xlabel("Time [days]", fontsize=15)
-    plt.ylabel("Nr. of individuals", fontsize=15)
-    plt.xticks(fontsize=13);plt.yticks(fontsize=13)
-    plt.tight_layout()
-
-    if save_plot:
-        print('\nSaving plot for method: %s, T=%g, b=%g' %(method, T, b))
-        plt.savefig('Results/SIR_%s_T[%g]_b[%g]'% (method, T, b))
-    else:
-        plt.show()
-
-
-for i in range(len(b_list)):
-    S, I, time  = RK4(b_list[i], S_0, I_0, fS, fI, n, T=T)
-    R           = N - S - I
-    plot_SIR(time, b_list[i], S, I, R, T, method='RK4', save_plot=True)
+P.plot_SIR(time, b, S, I, R, T, method='RK4')
+'''
--- Project5/main.py
+++ Project5/main.py
@@ -1,9 +1,8 @@
 """
 FYS4150 - Computational Physics
 
-Project 5 - Disease Modelling 
+Project 5 - Disease Modelling
 """
-
 
 import sys, os, time, argparse
 
@@ -11,6 +10,25 @@ import matplotlib.pyplot as plt
 import numpy             as np
 import pandas            as pd
 
+import RK4_disease       as RK4
+import plots             as P
+
+
+# Hvor skal konstanter staa igjen..?
+
+a       = 4         # Rate of transmission
+c       = 0.5       # Rate of immunity loss
+bA      = 1         # Rate of recovery for population A
+bB      = 2         # Rate of recovery for population B
+bC      = 3         # Rate of recovery for population C
+bD      = 4         # Rate of recovery for population D
+b_list  = [bA, bB, bC, bD]
+
+T   = 30            # Days
+n   = 1000
+N   = 400           # Nr of individuals in population
+S_0 = 300           # Initial number of susceptible
+I_0 = 100           # Initial number of infected
 
 
 if __name__ == '__main__':
@@ -19,10 +37,10 @@ if __name__ == '__main__':
     exercise  = parser.add_mutually_exclusive_group()
 
     exercise.add_argument('-a', '--RK4', action="store_true", help="RK4")
-    exercise.add_argument('-b', '--MC', action="store_true", help="Monte-Carlo")
-    exercise.add_argument('-c', '--VD', action="store_true", help="Vital Dynamics")
-    exercise.add_argument('-d', '--SV', action="store_true", help="Seasonal Variation")
-    exercise.add_argument('-e', '--VC', action="store_true", help="Vaccination")
+    exercise.add_argument('-b', '--MC',  action="store_true", help="Monte-Carlo")
+    exercise.add_argument('-c', '--VD',  action="store_true", help="Vital Dynamics")
+    exercise.add_argument('-d', '--SV',  action="store_true", help="Seasonal Variation")
+    exercise.add_argument('-e', '--VC',  action="store_true", help="Vaccination")
 
     if len(sys.argv) <= 1:
         sys.argv.append('--help')
@@ -32,6 +50,33 @@ if __name__ == '__main__':
     exB  = args.MC
     exC  = args.VD
     exD  = args.SV
-    exE  = args.VC 
+    exE  = args.VC
 
     print(parser.description)
+
+
+    if exA:
+
+        print('\nExercise A')
+
+        # Make RK4 simulation for 4 populations, with b=[bA, bB, bC, bD]
+        for i in range(len(b_list)):
+            S, I, time  = RK4.RK4(a, b_list[i], c, S_0, I_0, RK4.fS, RK4.fI, N, T, n)
+            R           = N - S - I
+            P.plot_SIR(time, b_list[i], S, I, R, T, method='RK4', save_plot=True)
+
+    if exB:
+
+        print('\nExercise B')
+
+    if exC:
+
+        print('\nExercise C')
+
+    if exD:
+
+        print('\nExercise D')
+
+    if exE:
+
+        print('\nExercise E')
