ed5c38e405963403227861486f73fb54d67e7a2b
aronjn <lagnadslaus@gmail.com>
2020-12-16 00:51:08 +0100

    combined

--- Project5/RK4_MonteCarlo_v2.py
+++ Project5/RK4_MonteCarlo_v2.py
@@ -6,7 +6,7 @@ import plots             as P
 e  = 0.25       # Birth rate
 d  = 0.2        # Death rate
 dI = 0.35       # Death rate of infected people due to the disease
-f  = 100        # Vaccination rate
+f  = 1        # Vaccination rate
 
 
 # Runge-Kutta 4
@@ -164,32 +164,62 @@ def RK4(a_in, b, c, x0, y0, z0, N, T, n, fx, fy, fz=None, Basic=False, Vital=Fal
                 z[i+1] = N - x[i] - y[i]  #maybe add this line to basic instead of doing R = N-S-I outside
                 t[i+1] = t[i] + dt
 
-        if CombinedModel:
+    if CombinedModel:
+        t_v = T/2  #start vaccination after half the total run time
+        for i in range(n-1):
+            #setting the transmission rate a, which varies with time
+            a0    = a_in
+            A     = 4    #max(a) = 4, min(a)= -4
+            omega = 0.5  #a is at max in beginning and end of year (winter)
+            a     = A*np.cos(omega*t[i]) + a0
 
-            # NB... Vaksine er ikke med her 
+            if t[i] >= t_v:  #vital + seasonal + vaccines
 
-            for i in range(n-1):
-                #setting the transmission rate a, which varies with time
-                a0    = a_in
-                A     = 4    #max(a) = 4, min(a)= -4
-                omega = 0.5  #a is at max in beginning and end of year (winter)
-                a     = A*np.cos(omega*t[i]) + a0
+                kx1 = dt*fx(a, b, c, N, x[i], y[i], z[i], combined=True)
+                ky1 = dt*fy(a, b, c, N, x[i], y[i], z[i], combined=True)
+                kz1 = dt*fz(a, b, c, N, x[i], y[i], z[i], combined=True)
 
-                kx1 = dt*fx(a, b, c, N, x[i], y[i], combined=True)
-                ky1 = dt*fy(a, b, c, N, x[i], y[i], combined=True)
+                kx2 = dt*fx(a, b, c, N, x[i] + kx1/2, y[i] + ky1/2, z[i] + ky1/2, combined=True)
+                ky2 = dt*fy(a, b, c, N, x[i] + kx1/2, y[i] + ky1/2, z[i] + kz1/2, combined=True)
+                kz2 = dt*fz(a, b, c, N, x[i] + kx1/2, y[i] + ky1/2, z[i] + kz1/2, combined=True)
 
-                kx2 = dt*fx(a, b, c, N, x[i] + kx1/2, y[i] + ky1/2, combined=True)
-                ky2 = dt*fy(a, b, c, N, x[i] + kx1/2, y[i] + ky1/2, combined=True)
+                kx3 = dt*fx(a, b, c, N, x[i] + kx2/2, y[i] + ky2/2, z[i] + kz2/2, combined=True)
+                ky3 = dt*fy(a, b, c, N, x[i] + kx2/2, y[i] + ky2/2, z[i] + kz2/2, combined=True)
+                kz3 = dt*fz(a, b, c, N, x[i] + kx2/2, y[i] + ky2/2, z[i] + kz2/2, combined=True)
 
-                kx3 = dt*fx(a, b, c, N, x[i] + kx2/2, y[i] + ky2/2, combined=True)
-                ky3 = dt*fy(a, b, c, N, x[i] + kx2/2, y[i] + ky2/2, combined=True)
-
-                kx4 = dt*fx(a, b, c, N, x[i] + kx3, y[i] + ky3, combined=True)
-                ky4 = dt*fy(a, b, c, N, x[i] + kx3, y[i] + ky3, combined=True)
+                kx4 = dt*fx(a, b, c, N, x[i] + kx3, y[i] + ky3, z[i] + kz3, combined=True)
+                ky4 = dt*fy(a, b, c, N, x[i] + kx3, y[i] + ky3, z[i] + kz3, combined=True)
+                kz4 = dt*fz(a, b, c, N, x[i] + kx3, y[i] + ky3, z[i] + kz3, combined=True)
 
                 x[i+1] = x[i] + (kx1 + 2*(kx2 + kx3) + kx4)/6
                 y[i+1] = y[i] + (ky1 + 2*(ky2 + ky3) + ky4)/6
+                z[i+1] = z[i] + (kz1 + 2*(kz2 + kz3) + kz4)/6
                 t[i+1] = t[i] + dt
+
+            else: #vital + seasonal
+
+                kx1 = dt*fx(a, b, c, N, x[i], y[i], z[i], vital=True)
+                ky1 = dt*fy(a, b, c, N, x[i], y[i], z[i], vital=True)
+                kz1 = dt*fz(a, b, c, N, x[i], y[i], z[i], vital=True)
+
+                kx2 = dt*fx(a, b, c, N, x[i] + kx1/2, y[i] + ky1/2, z[i] + ky1/2, vital=True)
+                ky2 = dt*fy(a, b, c, N, x[i] + kx1/2, y[i] + ky1/2, z[i] + kz1/2, vital=True)
+                kz2 = dt*fz(a, b, c, N, x[i] + kx1/2, y[i] + ky1/2, z[i] + kz1/2, vital=True)
+
+                kx3 = dt*fx(a, b, c, N, x[i] + kx2/2, y[i] + ky2/2, z[i] + kz2/2, vital=True)
+                ky3 = dt*fy(a, b, c, N, x[i] + kx2/2, y[i] + ky2/2, z[i] + kz2/2, vital=True)
+                kz3 = dt*fz(a, b, c, N, x[i] + kx2/2, y[i] + ky2/2, z[i] + kz2/2, vital=True)
+
+                kx4 = dt*fx(a, b, c, N, x[i] + kx3, y[i] + ky3, z[i] + kz3, vital=True)
+                ky4 = dt*fy(a, b, c, N, x[i] + kx3, y[i] + ky3, z[i] + kz3, vital=True)
+                kz4 = dt*fz(a, b, c, N, x[i] + kx3, y[i] + ky3, z[i] + kz3, vital=True)
+
+                x[i+1] = x[i] + (kx1 + 2*(kx2 + kx3) + kx4)/6
+                y[i+1] = y[i] + (ky1 + 2*(ky2 + ky3) + ky4)/6
+                z[i+1] = z[i] + (kz1 + 2*(kz2 + kz3) + kz4)/6
+                t[i+1] = t[i] + dt
+
+
 
 
     return x,y,z,t
@@ -203,9 +233,9 @@ def fS(a, b, c, N, S, I, R=None, vital=False, vaccine=False, combined=False):
         temp = c*R - a*S*I/N - d*S + e*N
     elif vaccine:
         R = N - S - I       # Blir denne riktig..???? Sendes ikke R inn?
-        temp = c*R - a*S*I/N - f # eller fS?
+        temp = c*R - a*S*I/N - f*S # eller fS?
     elif combined:
-        temp = c*R - a*S*I/N - d*S + e*N - f # eller fS?
+        temp = c*R - a*S*I/N - d*S + e*N - f*S # eller fS?
     else:
         temp = c*(N-S-I) - a*S*I/N
 
@@ -234,9 +264,9 @@ def fR(a, b, c, N, S, I, R, vital=False, vaccine=False, combined=False):
         temp = b*I - c*R - d*R
     elif vaccine:
         R = N - S - I       # Blir denne riktig..???? Sendes ikke R inn?
-        temp = b*I - c*R + f # eller fS?
+        temp = b*I - c*R + f*S # eller fS?
     elif combined:
-        temp = b*I - c*R - d*R + f # eller fS?
+        temp = b*I - c*R - d*R + f*S # eller fS?
     else:
         temp = 0
 
@@ -327,6 +357,7 @@ def MC(a_in, b, c, S_0, I_0, R_0, N, T, vitality=False, seasonal=False, vaccine=
             S[i+1] += 1
 
         if vitality:
+
             #death rate d in general population S, I and R
             r_dS = np.random.random()
             if r_dS < (d*S[i]*dt):     #d*S*dt = probability of one individual dying in S category
